50a570a055036768bf4d0e3cf73fe9ba21d73b42
wlals8327@naver.com <Me@DESKTOP-DAKEHQT>
2018-07-16 10:43:58 +0800

    test

--- summerproject/src/summerproject/TestGithub.java
+++ summerproject/src/summerproject/TestGithub.java
@@ -5,7 +5,6 @@ public class TestGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test github ");
-		System.out.println("dd");
 	}
 
 }
